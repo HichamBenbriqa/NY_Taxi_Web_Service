ef8e681637e5ed98d9e92a3c1401b17f20e413aa
HichamBenbriqa <hichambenbriqa@gmail.com>
2023-12-24 18:22:13 -0300

    add main.yml

--- deployment/deploy.py
+++ deployment/deploy.py
@@ -193,8 +193,7 @@ class Deployer:
 
 if __name__ == "__main__":
     print(AWS_REGION)
-    print(os.getenv())
-    sagemaker_client = boto3.client(service_name="sagemaker", region_name="us-east-1")
+    sagemaker_client = boto3.client(service_name="sagemaker", region_name=AWS_REGION)
     model_artifacts_tar = "model.tar.gz"
     boto_session = boto3.session.Session()
 
